a80755edad472e07b8e184304703eafdcda91743
AnmolMishra6568 <anmolmishra6568@gmail.com>
2023-08-01 12:30:56 +0530

    Note making

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header=()=> {
+  return (<>
+    <div className="header">
+        <img src="https://icon-library.com/images/google-keep-icon/google-keep-icon-15.jpg" alt="logo" width='70' height='50' />
+        <h1>KEEP NOTES</h1>
+    </div>
+  </>
+  )
+}
+
+export default Header;
